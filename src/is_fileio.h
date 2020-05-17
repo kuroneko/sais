2ab85a2e449ec0b572e9016748b539d92e8f33b9
Chris Collins <kuroneko@sysadninjas.net>
2020-05-17 10:56:51 +1000

    Include header include guard on is_fileio.h

--- src/is_fileio.h
+++ src/is_fileio.h
@@ -1,3 +1,6 @@
+#ifndef IS_FILEIO_H
+#define IS_FILEIO_H
+
 FILE *myopen(const char *fname, const char *flags);
 int read_line(FILE *in, char *out1, char *out2);
 int read_line1(FILE *in, char *out1);
@@ -7,3 +10,5 @@ void ik_print_log(const char *ln, ...);
 extern FILE *logfile;
 extern int last_logdate;
 extern char moddir[256];
+
+#endif /* IS_FILEIO_H */
