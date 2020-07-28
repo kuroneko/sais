453ce618b25402ae334b0b14fbea3104ac590914
Chris Collins <kuroneko@sysadninjas.net>
2020-07-28 19:17:01 +1000

    CLion Project File Update

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,20 @@
+#if ($HEADER_COMMENTS)
+/* Strange Adventures in Infinite Space GPL
+ * Copyright (C) 2005 Richard Carlson, Iikka Keranen and William Sears
+ * Copyright (C) 2020 Christopher Collins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#end
+
