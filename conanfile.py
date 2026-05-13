9f83ac253a89c3b5aa6273337649010d1d6b669c
Chris Collins <kuroneko-git@sysadninjas.net>
2026-05-13 15:06:24 +1000

    Update to build using conan2

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,38 @@
-from conans import ConanFile, CMake, tools
-
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
 
 class SaisConan(ConanFile):
     name = "sais"
+    version = "1.7.0"
     license = "GPL"
     settings = "os", "compiler", "build_type", "arch"
+    tool_requires = [
+        'cmake/4.2.3',
+    ]
     requires = [
-        'sdl/2.26.1',
-        'sdl_mixer/2.0.4',
-        'physfs/3.0.2',
+        'sdl/2.32.10',
+        'sdl_mixer/2.8.1',
+        'physfs/3.2.0',
     ]
     default_options = {
-        "*:shared": False,
-        "sdl2:iconv": False,
-        "sdl2_mixer:fluidsynth": False,
-        "sdl2_mixer:mad": False,
-        "sdl2_mixer:mikmod": False,
-        "sdl2_mixer:modplug": False,
-        "sdl2_mixer:mpg123": False,
-        "sdl2_mixer:opus": False,
-        "sdl2_mixer:flac": False,
-        "sdl2_mixer*:tinymidi": False,
-        "sdl2_mixer:wav": True,
+        "*/*:shared": False,
+        "sdl/*:iconv": False,
+        "sdl_mixer/*:fluidsynth": False,
+        "sdl_mixer/*:mad": False,
+        "sdl_mixer/*:mikmod": False,
+        "sdl_mixer/*:modplug": False,
+        "sdl_mixer/*:mpg123": False,
+        "sdl_mixer/*:opus": False,
+        "sdl_mixer/*:flac": False,
+        "sdl_mixer/*:tinymidi": False,
+        "sdl_mixer/*:wav": True,
     }
-    generators = "cmake"
+    generators = "CMakeConfigDeps", "CMakeToolchain"
 
     def configure(self):
         # SDL2 on macOS requires iconv
         if self.settings.os == "Macos":
             self.options["sdl2"].iconv = True
+
+    def layout(self):
+        cmake_layout(self)
